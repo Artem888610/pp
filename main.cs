37583f55172530da2b340fb18846e449355300a8
Артём <monin_artem@inbox.ru>
2026-03-11 14:25:05 +0300

    Now Hello a personalize.

--- main.cs
+++ main.cs
@@ -4,7 +4,10 @@ namespace Git
     {
         static void Main(string[] args)
         {
-           System.Console.Writeline("Hello Git!")
+           System.Console.Writeline("Enter your name: ");
+           string name = Console.ReadLine();
+           System.Console.Writeline(name.length); // debug line
+           System.Console.Writeline($"Hello {name}!");
         }
     }
 }
