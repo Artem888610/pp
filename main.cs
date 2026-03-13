b0d8286ef5ef8239d8d49ef9ebcc9b6fe28382f7
Артём <monin_artem@inbox.ru>
2026-03-13 08:48:06 +0300

    Adder a request for the user's last name
    Signed-off-by: Alex <alex@exanple.com>

--- main.cs
+++ main.cs
@@ -4,10 +4,11 @@ namespace Git
     {
         static void Main(string[] args)
         {
-           System.Console.Writeline("Enter your name: ");
-           string name = Console.ReadLine();
-           System.Console.Writeline(name.length); // debug line
-           System.Console.Writeline($"Hello {name}!");
+           System.Console.Writeline("Enter your first name: "); 
+           string first_name = Console.ReadLine();
+           System.Console.Writeline("Now enter your last name: ")
+           string last_name = Console.ReadLine();
+           System.Console.Writeline($"Hello {first name} {last name}!");
         }
     }
 }
